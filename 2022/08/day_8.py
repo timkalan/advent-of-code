478079ea6c6b3ff3fa17beabf1d60464a66da3d6
Tim Kalan <timkalan99@gmail.com>
2022-12-07 13:05:34 +0100

    day 8

---
+++ 2022/08/day_8.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+
+
+with open('day_8.in') as f:
+    commands = f.read().split('\n')
+    full_path = []
+    directories = defaultdict(int)
+
+    for cmd in commands:
+        match cmd.split():
+            case ['$', 'cd', '..']:
+                full_path.pop()
+            case ['$', 'cd', p]:
+                full_path.append(p)
+            case ['$', 'ls']:
+                pass
+            case ['dir', p]:
+                pass
+            case [s, f]:
+                directories[tuple(full_path)] += int(s)
+                path = full_path[:-1]
+                while path:
+                    directories[tuple(path)] += int(s)
+                    path.pop()
+
+
+total = sum([_ for _ in directories.values() if _ <= 100000])
+
+needed_space = directories[('/', )] - 40000000
+to_delete = min([_ for _ in directories.values() if _ > needed_space])
+
+print(directories[('/', )])
+
+print('Part 1:')
+print(total)
+print()
+print('Part 2:')
+print(to_delete)
+
+
+
+# with open('day_8.in') as f:
+#     commands = f.read().split('\n')
+#     directories = {}
+#     for i, output in enumerate(commands):
+#         if output.startswith('$ cd') and not output.endswith('..'):
+#             current_dir = output[5:]
+#             # current_dir.append(output)
+#             directories[current_dir] = {}
+
+#         elif not output.startswith('$'):
+#             if not output.startswith('dir'):
+#                 size, filename = output.split()
+#                 directories[current_dir][filename] = {int(size)}
+#             else:
+#                 directories[current_dir][output.split()[1]] = {}
+
+#         elif output.endswith('..'):
+#             # current_dir.pop()
+#             continue
