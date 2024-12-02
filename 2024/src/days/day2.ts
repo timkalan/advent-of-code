4a4fcabe85ac6ddf5824c48b0603e4a42ed191a0
Tim Kalan <timkalan99@gmail.com>
2024-12-02 20:29:43 +0100

    2024 day 2

---
+++ 2024/src/days/day2.ts
@@ -0,0 +1,35 @@
+import { readInput } from "../utils";
+
+const input = readInput("day2.txt");
+
+const reports = input
+    .split("\n")
+    .map(line => line.trim().split(" ").map(Number))
+
+const reasonableIncrease = (value: number, i: number, report: number[]) =>
+    i === 0 || (1 <= value - report[i - 1] && value - report[i - 1] <= 3);
+
+const reasonableDecrease = (value: number, i: number, report: number[]) =>
+    i === 0 || (1 <= report[i - 1] - value && report[i - 1] - value <= 3);
+
+const isSafe = (report: number[]) =>
+    report.every(reasonableIncrease) || report.every(reasonableDecrease);
+
+const isAlmostSafe = (report: number[]) =>
+    report.some((_, i) => isSafe(report.filter((_, j) => i != j)));
+
+export function solvePart1(): number {
+    const result = reports
+        .filter(isSafe)
+        .length;
+
+    return result;
+}
+
+export function solvePart2(): number {
+    const result = reports
+        .filter(isAlmostSafe)
+        .length;
+
+    return result;
+}
